bdcb5183697489e8924b4f5a234af95ba011bc3b
emma <emanuelfernandoquero@gmail.com>
2022-07-02 14:57:16 -0300

    prueba

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ItemDetail = ({ product }) => {
     return (
-        <div>
+        <div className='container'>
             <img src={product.img} alt={product.name} />
             <div>
                 <h1>{product.name}</h1>
-                <h2>{product.description}</h2>
+                <p>{product.description}</p>
                 <h3>{product.stock}</h3>
             </div>
         </div>
--- src/mocks/fakApi.js
+++ src/mocks/fakApi.js
@@ -7,7 +7,7 @@ const products = [
     {id:'04', name: 'Iphone12', description: "valor 250.000$",img:'/imgs/iphone12.jfif'  ,stock: 1},
 ] 
 const product = {
-    id:'04', name: 'Iphone12', description: "valor 250.000$",img:'/imgs/iphone12.jfif'  ,stock: 1,
+    id:'04', name: 'Apple iPhone12 128 gb', description: "Descripción: El iPhone 12 tiene una espectacular pantalla Super Retina XDR de 6.1 pulgadas . Un frente de Ceramic Shield, cuatro veces más resistente a las caídas. Modo Noche en todas las cámaras, para que puedas tomar fotos increíbles con poca luz. Grabación, edición y reproducción de video en Dolby Vision con calidad cinematográfica. Y el potente chip A14 Bionic. Además, es compatible con los nuevos accesorios MagSafe, que se acoplan fácilmente a tu iPhone y permiten una carga inalámbrica más rápida . Que comience la diversión.",img:'/imgs/detalle.jfif'  ,stock: 1
 
 }
 
